81c9861626b456b25a4c19aa84fd11e67af4386f
lyucheng.zhu <stephenzhu1995@gmail.com>
2019-04-02 16:22:37 -0400

    client part added

---
+++ client/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/medium.css';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
+
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import App from './App.js';
+
+import { store } from './redux/store';
+
+import { getUser } from './redux/actions/actions'
+
+if(localStorage.Auth) {
+    console.log('first dispatch')
+    //console.log(localStorage.Auth)
+    // update localstorage
+    store.dispatch({type: 'SET_USER', user: JSON.parse(localStorage.Auth)})
+
+    var _id = JSON.parse(localStorage.Auth)._id
+    getUser(_id).then((res)=>{
+        //console.log(JSON.parse(res))
+        store.dispatch({type: 'SET_USER', user: res})
+    })
+}
+
+ReactDOM.render((
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" component={App} />
+            </Switch>
+        </BrowserRouter>
+    </Provider>
+), document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
